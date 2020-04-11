ff7a80cbca50bbdcb934386e2406e67c3b34d3b0
Irina Voropayeva <i.vvrpv@gmail.com>
2020-04-11 21:15:12 +0300

    ASP.NET MVC Ode To Food application implementing

---
+++ OdeFood.Web/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="OdeFood.Web.MvcApplication" Language="C#" %>
---
+++ OdeToFood.Data/Services/InMemoryRestaurantData.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using OdeToFood.Data.Models;
+
+namespace OdeToFood.Data.Services
+{
+    public class InMemoryRestaurantData : IRestaurantData
+    {
+        private List<Restaurant> restaurants;
+
+        public InMemoryRestaurantData()
+        {
+            restaurants = new List<Restaurant>()
+            {
+                new Restaurant() { Id = 1, Name = "Pizza", Cuisine = CuisineType.Italian},
+                new Restaurant() { Id = 1, Name = "Franch", Cuisine = CuisineType.French},
+                new Restaurant() { Id = 1, Name = "Mango Groove", Cuisine = CuisineType.Indian},
+            };
+        }
+
+        public IEnumerable<Restaurant> GetAll()
+        {
+            return restaurants.OrderBy(r => r.Name);
+        }
+
+        public Restaurant Get(int id)
+        {
+            return restaurants.FirstOrDefault(r => r.Id == id);
+        }
+    }
+}
